d76b398d23f53417810f3c50654f16a58e28cad4
LuisToscano74. <[200~163039949+LuisToscano74@users.noreply.github.com~>
2024-12-02 17:04:49 -0600

    Iniciamos con clases en el DOM

---
+++ 04-Clases/script.js
@@ -0,0 +1,7 @@
+/* Accedemos al primer elemento de la lista */
+
+const primerBotana = document.getElementsByTagName('li');
+
+console.log(primerBotana[2]);
+
+/* commit -m "Iniciamos con clases en el DOM" */
